d9cc1a52dd043718babd1b7c3d512a8ac8df47fd
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-09-26 22:43:26 -0700

    Add files via upload

---
+++ linkedList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class linkedList {
+    public static void main(String args[])
+    {
+        LinkedList<String> l1 = new LinkedList<String>();
+        l1.add("Priyam");
+        l1.add("Rao");
+        l1.add("is");
+        l1.add("coding");
+
+        System.out.println(l1);
+    }
+}
