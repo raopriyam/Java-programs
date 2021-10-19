6f99d4502cdaa4400659975982e58ce1cac893a1
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-10-18 21:27:27 -0700

    Update array.java

--- array.java
+++ array.java
@@ -2,6 +2,7 @@ public class array {
     public static void main(String args[]){
         int arr1[] = new int[]{1,2,3,4,5,6,7,8,9,10};
         int arr2[] = new int[]{1,2,3,4,5,6,7,8,9,10};
+        int arr3[] = new int[]{1,2,3,4,5,6,7,8,9,10};
         int i;
         for(i=0;i<arr1.length;i++)
         System.out.println(arr1[i]);
