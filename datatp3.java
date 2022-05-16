8ec0cf70f5d951a71045dcadb049d5f793f6cbef
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2022-05-16 01:43:04 -0500

    Add files via upload

---
+++ datatp3.java
@@ -0,0 +1,8 @@
+package packages;
+
+public class datatp3 {
+    boolean val = true;
+    public boolean show(){
+        return val;
+    }
+}
---
+++ datatype1.java
@@ -0,0 +1,8 @@
+package packages;
+
+public class datatype1 {
+    double b = 1.3;
+    public void show(){
+        System.out.println("value of b: "+b);
+    }
+}
