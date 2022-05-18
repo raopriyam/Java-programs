5143cc280d34809bd830e0e6eff865923a21139e
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2022-05-18 01:22:57 -0500

    Add files via upload

---
+++ PrintElements.java
@@ -0,0 +1,12 @@
+package packages;
+
+import java.sql.Array;
+
+public class PrintElements<array1> {
+    public void printElements(int[] array1){
+        for(int i=0;i<array1.length;i++){
+            System.out.println(array1[i]);
+        }
+    }
+
+}
---
+++ printChars.java
@@ -0,0 +1,14 @@
+package packages;
+
+public class printChars {
+    String s2 = "My name is Priyam Rao";
+    public void PrintChars(){
+        int i = 0;
+        for(i=0;i<s2.length();i++){
+
+
+        }
+        System.out.println("Length of the given string: "+i);
+    }
+
+}
