86c424f292f3d90620dc0bb77b70ca9c3091ad9e
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2022-05-27 00:03:19 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -10,35 +10,26 @@ import packages.char1;
 import packages.string1;
 import packages.printChars;
 import packages.PrintElements;
+import packages.Calculator;
+
+import package2.pattern1;
+import package2.pattern2;
+import package2.pattern3;
+
 
 
 public class Main {
     public static void main(String args[]){
-        PrintStatements pr1 = new PrintStatements();
-        variable1 var1 = new variable1();
-        varSubtract vs1 = new varSubtract();
-        VarMultiply vm1 = new VarMultiply();
-        variable2 var2 = new variable2();
-        datatype1 dt1 = new datatype1();
-        datatype2 dt2 = new datatype2();
-        datatp3 dt3 = new datatp3();
-        char1 ch1 = new char1();
-        string1 s1 = new string1();
-        printChars prc1 = new printChars();
-        PrintElements pe1 = new PrintElements();
+
+        Calculator cal1 = new Calculator();
+        pattern1 pt1 = new pattern1();
+        pattern2 pt2 = new pattern2();
+        pattern3 pt3 = new pattern3();
 
 
-        pr1.Show();
-        System.out.println(var1.Add(2,6));
-        System.out.println(vs1.Sub(7,3));
-        System.out.println(vm1.Mul(9,76));
-        var2.Show();
-        dt1.show();
-        dt2.Result();
-        dt3.show();
-        System.out.println(ch1.displayChar());
-        System.out.println(s1.strShow());
-        prc1.PrintChars();
-        pe1.printElements( new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+
+        pt1.pattern1();
+        pt2.show();
+        pt3.show();
     }
 }
