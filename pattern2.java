c4de3242df2a213b09c1db38d3b6fe26550fecd2
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2022-05-27 00:02:51 -0500

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,12 @@
+package package2;
+
+public class pattern2 {
+    public void show(){
+        for(int i = 1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
